78624b1a9a078bc9699d04ecfbec50b009878558
BrianBontempi <brian.bont54@gmail.com>
2023-12-24 21:59:29 +0100

    take ticket element in js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ const confirmButton = document.getElementById("confirm-button")
 const resetButton = document.getElementById("reset-button")
 const buyButton = document.getElementById("buy-button")
 
+// ! elementi dei ticket
+
+const ticketSection = document.getElementById("ticket")
+const costumerElement = document.getElementById("costumer-name")
+const rateElement = document.getElementById("rate")
+const carElement = document.getElementById("car")
+const cpElement = document.getElementById("CP")
+const priceElement = document.getElementById("total")
+
