2257ce5a21949798d1a41885dd090aacd3162cba
BrianBontempi <brian.bont54@gmail.com>
2023-12-24 21:53:16 +0100

    take element in js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 console.log("JS OK")
 
+// ! elementi del form
+const nameField = document.getElementById("name")
+const ageField = document.getElementById("age")
+const kmsField = document.getElementById("kms")
+
+// ! bottoni
+
+const confirmButton = document.getElementById("confirm-button")
+const resetButton = document.getElementById("reset-button")
+const buyButton = document.getElementById("buy-button")
+
