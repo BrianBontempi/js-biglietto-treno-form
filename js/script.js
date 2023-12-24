624d38f680e78efbe00423cfe9da8c011d47a4a7
BrianBontempi <brian.bont54@gmail.com>
2023-12-24 22:08:54 +0100

    add event listener confirm button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 console.log("JS OK")
 
 // ! elementi del form
-const nameField = document.getElementById("name")
-const ageField = document.getElementById("age")
-const kmsField = document.getElementById("kms")
+const nameField = document.getElementById("name");
+const ageField = document.getElementById("age");
+const kmsField = document.getElementById("kms");
 
 // ! bottoni
 
-const confirmButton = document.getElementById("confirm-button")
-const resetButton = document.getElementById("reset-button")
-const buyButton = document.getElementById("buy-button")
+const confirmButton = document.getElementById("confirm-button");
+const resetButton = document.getElementById("reset-button");
+const buyButton = document.getElementById("buy-button");
 
 // ! elementi dei ticket
 
-const ticketSection = document.getElementById("ticket")
-const costumerElement = document.getElementById("costumer-name")
-const rateElement = document.getElementById("rate")
-const carElement = document.getElementById("car")
-const cpElement = document.getElementById("CP")
-const priceElement = document.getElementById("total")
+const ticketSection = document.getElementById("ticket");
+const costumerElement = document.getElementById("costumer-name");
+const rateElement = document.getElementById("rate");
+const carElement = document.getElementById("car");
+const cpElement = document.getElementById("cp");
+const priceElement = document.getElementById("total");
 
+// ! variabili standard
+
+const priceKm = 0.21;
+let rateName = "Tariffa Standard"
+
+// ! event listener
+
+confirmButton.addEventListener("click", function(){
+    // ! recupero i valori dal form
+   const nameValue = nameField.value.trim();
+   const kmsValue = parseInt(kmsField.value);
+   const ageValue = ageField.value;
+   console.log(nameValue, kmsValue, ageValue)
+})
